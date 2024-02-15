0a6a9849f3a4a8654c1f89fad283a1dab678cf0d
justinyeh1995 <justinyeh1995@gmail.com>
2024-02-15 01:23:12 +0000

    add utils

---
+++ CybORG/Tutorial/Mininet/utils/action_translator.py
@@ -0,0 +1,9 @@
+def transform_discover_remote_systems(subnet):
+    pass
+
+def transform_discover_network_services(subnet):
+    pass
+
+def transform_exploit_network_services(ip_address, port):
+    pass
+    
