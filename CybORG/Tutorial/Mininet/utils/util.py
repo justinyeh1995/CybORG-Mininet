1d404e24f0435a9bca31b83620b35fa88ac16d1c
justinyeh1995 <justinyeh1995@gmail.com>
2024-02-21 22:18:59 +0000

    return action success or not as well

--- CybORG/Tutorial/Mininet/utils/util.py
+++ CybORG/Tutorial/Mininet/utils/util.py
@@ -14,13 +14,17 @@ def translate_remove(ip_address, port):
     pass
     
 
-def parse_action(cyborg, action_str, agent, host_map, ip_map):
+def parse_action(cyborg, action_str, agent, ip_to_host_map):
     action_type = action_str.split(" ")[0]
     target_host = ""
-    if cyborg.get_observation(agent)['success'].__str__() == 'TRUE':
+    isSuccess = cyborg.get_observation(agent)['success'].__str__()
+    if isSuccess == 'TRUE':
         action_str_split = action_str.split(" ")
         n = len(action_str_split)
         target_host = action_str_split[-1] if n > 1 else target_host
+        print(target_host)
         # Update target host if it's an IP address to get the hostname
-        target_host = ip_map.get(target_host, target_host) if target_host in ip_map else target_host
-    return target_host, action_type
+        target_host = ip_to_host_map.get(target_host, target_host) #if target_host in ip_to_host_map else target_host
+        if action_type == "DiscoverRemoteSystems":
+            print("Hey DiscoverRemoteSystems")
+    return target_host, action_type, isSuccess
