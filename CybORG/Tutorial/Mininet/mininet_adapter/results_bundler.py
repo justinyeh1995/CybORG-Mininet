d82ac8421aeff3f1545d204dc39f278f4b8268db
justinyeh1995 <justinyeh1995@gmail.com>
2024-03-05 01:50:15 +0000

    updating parsing functions

--- CybORG/Tutorial/Mininet/mininet_adapter/results_bundler.py
+++ CybORG/Tutorial/Mininet/mininet_adapter/results_bundler.py
@@ -1,7 +1,48 @@
+import re
 import traceback 
+from typing import List, Dict
 
+def parse_nmap_network_scan(text, target) -> List:
+    res = {'success': True}
+    subnet = target
+    ip_addresses = re.findall(r'Nmap scan report for (\d+\.\d+\.\d+\.\d+)', text)
+    res[subnet] = ip_addresses
+    return res
+    
+def parse_nmap_port_scan(text, target) -> List:
+    res = {'success': True}
+    ip = target
+    # Regular expression to match the port information
+    port_info_regex = re.compile(r'(\d+)/(\w+)\s+open\s+(\w+)\s+(.+)')
+    
+    # Find all matches
+    matches = port_info_regex.findall(nmap_output)
+    
+    # Process matches
+    ports = []
+    for match in matches:
+        port_number, protocol, service_name, version = match
+        ports.append({
+            'port': port_number,
+            'protocol': protocol,
+            'service': service_name,
+            'version': version.strip()
+        })    
+    res[ip] = ports    
+    return res
+    
 class ResultsBundler:
-    def bundle(self, raw_str):
+    def bundle(self, target, cyborg_action, isSuccess, mininet_cli_str) -> Dict:
         # @To-Do
-        return "dummy text"
+        if mininet_cli_str == "":
+            return {'success': False}
+        
+        if cyborg_action == "DiscoverRemoteSystems":
+            return parse_nmap_network_scan(mininet_cli_str, target)
+            
+        elif cyborg_action == "DiscoverRemoteSystems":
+            return parse_nmap_port_scan(mininet_cli_str, target)
+
+        return res
+        
         
