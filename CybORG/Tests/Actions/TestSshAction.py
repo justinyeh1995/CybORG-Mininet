f46bafb4481fcf082a8ab1f0aa601e00c4fec873
Harmon Nine <Harmon.Nine@gmail.com>
2024-03-09 13:20:43 -0600

    Have SshAction take a port as an argument, and have it deposit a malicious file on success

--- CybORG/Tests/Actions/TestSshAction.py
+++ CybORG/Tests/Actions/TestSshAction.py
@@ -1,7 +1,7 @@
 from CybORG.Emulator.Actions.SshAction import SshAction
 
 
-sshAction = SshAction("172.17.0.2", "velociraptor", "velociraptor")
+sshAction = SshAction("172.17.0.2", "velociraptor", "velociraptor", 22)
 #sshAction = SshAction("172.17.0.3", "sftp_user", "art54")
 
 observation = sshAction.execute(None)
