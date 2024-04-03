b710aedac4e3c5d6ee9ec9cdbe576e5c2cb56f3d
justinyeh1995 <justinyeh1995@gmail.com>
2024-04-03 23:56:50 +0000

    Run SSHAction & DeployDecoyAction as python script - if __name__ == main__

--- CybORG/Tutorial/Mininet/utils/ssh_action.py
+++ CybORG/Tutorial/Mininet/utils/ssh_action.py
@@ -1,7 +1,7 @@
 from CybORG.Emulator.Actions.SshAction import SshAction
 
 
-if __name__ == __main__:
+if __name__ == "__main__":
     
     sshAction = SshAction("172.17.0.2", "velociraptor", "velociraptor", 22)
     #sshAction = SshAction("172.17.0.3", "sftp_user", "art54")
