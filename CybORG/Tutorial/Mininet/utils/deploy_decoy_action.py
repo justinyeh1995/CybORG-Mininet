4d3529133b1be10e6e3717b2a614b3257c511df1
justinyeh1995 <justinyeh1995@gmail.com>
2024-04-03 23:50:45 +0000

    Run SSHAction & DeployDecoyAction as python script

---
+++ CybORG/Tutorial/Mininet/utils/deploy_decoy_action.py
@@ -0,0 +1,12 @@
+from CybORG.Emulator.Actions.DeployDecoyAction import DeployDecoy
+
+
+if __name__ == __main__:
+
+    deploy_decoy = DeployDecoy('172.17.0.3', 'velociraptor', 'velociraptor', 'apache', 80)
+    
+    observation = deploy_decoy.execute(None)
+
+    print(observation)
+
+    print("foo")
---
+++ CybORG/Tutorial/Mininet/utils/ssh_action.py
@@ -0,0 +1,13 @@
+from CybORG.Emulator.Actions.SshAction import SshAction
+
+
+if __name__ == __main__:
+    
+    sshAction = SshAction("172.17.0.2", "velociraptor", "velociraptor", 22)
+    #sshAction = SshAction("172.17.0.3", "sftp_user", "art54")
+    
+    observation = sshAction.execute(None)
+
+    print(observation)
+    
+    print("foo")
