42a943899cd45c607b1056b02f1036665a64b25a
justinyeh1995 <justinyeh1995@gmail.com>
2024-04-09 20:46:16 +0000

    discard using timeout on ssh_action and deploy_decoy_action

--- CybORG/Mininet/mininet_adapter/action_translator.py
+++ CybORG/Mininet/mininet_adapter/action_translator.py
@@ -39,7 +39,10 @@ class RedActionTranslator(ActionTranslator):
         elif action_type == "PrivilegeEscalate":
             print("Red Privilege Escalate")
             # @To-Do Not Implemented as of now
+            action = "sleep 1" # dummy
+            target = "" # dummy
         else:
+            print("Not Implemented Action...\nSleep for a sec")
             action = "sleep 1" # dummy
             target = "" # dummy
             
@@ -53,7 +56,7 @@ class BlueActionTranslator(ActionTranslator):
         super().__init__()  # Correctly calls the __init__ method of ActionTranslator
         self.decoy_bin_path = self.path + f'/Emulator/Velociraptor/Executables/Decoy'
         self.decoy_service_name_to_port = {
-            'DecoyApache': 80, 
+            'DecoyApache': 81, 
             'DecoySSHD': 22, 
             'DecoyVsftpd': 21, 
             'DecoyFemitter': 21, 
@@ -62,8 +65,8 @@ class BlueActionTranslator(ActionTranslator):
         }
         
     def translate(self, action_type, target_host, cyborg_to_mininet_host_map, mininet_host_to_ip_map) -> str:
-        host = cyborg_to_mininet_host_map['Defender'] # red host is always user0
-        timeout = 60
+        host = cyborg_to_mininet_host_map['Defender'] # blue host is always defender
+        timeout = 360
         # @To-Do code smells
         # blue host is undecided at the moment
         cmd = 'lan1h1 ping -c 1 lan2h2'
@@ -78,5 +81,5 @@ class BlueActionTranslator(ActionTranslator):
             action = f"/home/ubuntu/justinyeh1995/CASTLEGym/CybORG/castle.new.venv/bin/python3 {self.path}/Mininet/utils/deploy_decoy_action.py --ip" # @To-Do needs to be configurable in the future
             target = mininet_host_to_ip_map.get(target_host, cyborg_to_mininet_host_map['User0'])
             port = self.decoy_service_name_to_port.get(action_type, 80)
-            cmd = f"{host} echo 'nameserver 8.8.8.8' >> /etc/resolv.conf && timeout {timeout} {action} {target} --port {port}"
+            cmd = f"{host} {action} {target} --port {port}"
         return cmd
