083cfa03307288d78f28f41e90bad83099021e31
justinyeh1995 <justinyeh1995@gmail.com>
2024-04-11 19:02:22 +0000

    start adding tests

--- CybORG/Mininet/Tests/test_adapter.py
+++ CybORG/Mininet/Tests/test_adapter.py
@@ -1,7 +1,11 @@
 import pytest
 import inspect
 
-from CybORG.Tutorial.Mininet.MininetAdapter import MininetAdapter
+from CybORG.Mininet.MininetAdapter import MininetAdapter
 
-def test():
-    assert 1 == 1
+class TestMininetAdapter:
+    def __init__(self):
+        self.mininet_adapter = MininetAdapter()
+        
+    def test_topology(self):
+        assert 1 == 1
