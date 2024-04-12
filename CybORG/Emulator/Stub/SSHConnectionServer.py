891e3eb6dfd0dc1dac2c50450f607d9b1ba5f873
Harmon Nine <Harmon.Nine@gmail.com>
2024-04-12 10:43:39 -0500

    Updated SSHConnectionServer (works well)

--- CybORG/Emulator/Stub/SSHConnectionServer.py
+++ CybORG/Emulator/Stub/SSHConnectionServer.py
@@ -7,6 +7,8 @@ import paramiko
 from pathlib import Path
 import random
 import re
+import signal
+import socket
 import string
 import sys
 import time
@@ -19,18 +21,38 @@ class SSHConnectionServer:
     enable_bracketed_paste = "\x1b[?2004h"
     disable_bracketed_paste = "\x1b[?2004l\r"
 
-    def __init__(self, connection_key: str, hostname: str, username: str, private_key_file_path: Union[str, Path]):
+    def __init__(
+            self,
+            connection_key: str = None,
+            hostname: str = None,
+            username: str = None,
+            password: str = None,
+            private_key_file_path: Union[str, Path] = None,
+            client_port: int = 0,
+            server_port: int = 22
+        ):
 
         self.socket_path = Path(f"/tmp/ssh_connection_{connection_key}")
         self.hostname = hostname
         self.username = username
-        self.private_key = paramiko.RSAKey.from_private_key_file(str(private_key_file_path))
+        self.password = password
+        self.private_key = None if private_key_file_path is None \
+            else paramiko.RSAKey.from_private_key_file(str(private_key_file_path))
+        self.client_port = client_port
+        self.server_port = server_port
 
+        self.socket = None
         self.ssh_session = None
         self.interactive_shell = None
 
         self.shell_output_bytes = None
         self.start = None
+
+        signal.signal(signal.SIGINT, lambda signal, frame: self.handle_interrupt())
+
+    def handle_interrupt(self):
+        self.ssh_session.close()
+        sys.exit(0)
 
     @classmethod
     def get_marker(cls):
@@ -135,13 +157,22 @@ class SSHConnectionServer:
 
         return shell_output_text
 
-    def establish_ssh_session(self):
+    def establish_session(self):
         self.ssh_session = paramiko.SSHClient()
 
         self.ssh_session.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
-        self.ssh_session.connect(hostname=self.hostname, username=self.username, pkey=self.private_key)
-
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.socket.bind(("0.0.0.0", self.client_port))
+        self.socket.connect((self.hostname, self.server_port))
+        self.ssh_session.connect(
+            hostname=self.hostname,
+            password=self.password,
+            pkey=self.private_key,
+            username=self.username,
+            sock=self.socket
+        )
         self.interactive_shell = self.ssh_session.invoke_shell()
         self.interactive_shell.sendall("stty -echo\n".encode())
 
@@ -175,7 +206,7 @@ class SSHConnectionServer:
         socket = context.socket(zmq.REP)
         socket.bind(f"ipc://{self.socket_path}")
 
-        self.establish_ssh_session()
+        self.establish_session()
 
         while True:
             json_input = socket.recv_string()
@@ -193,6 +224,7 @@ class SSHConnectionServer:
 
             if json_data == "CLOSE":
                 self.ssh_session.close()
+                self.socket = None
                 self.send_response("CLOSED", socket)
                 socket.close()
                 self.socket_path.unlink()
@@ -209,10 +241,12 @@ class SSHConnectionServer:
             except Exception as e:
                 try:
                     self.ssh_session.close()
+
+                    self.socket = None
                 except Exception:
                     pass
 
-                self.establish_ssh_session()
+                self.establish_session()
                 self.send_response("RECONNECT", socket)
 
 
@@ -222,27 +256,36 @@ if __name__ == "__main__":
         description="Server for communicating over a persistent SSH connection"
     )
     parser.add_argument(
-        "connection_key",
-        nargs="?",
+        "-n", "--connection-key", required=True,
         help="key used to unique identify ssh connection"
     )
     parser.add_argument(
-        "hostname",
-        nargs="?",
+        "-m", "--hostname", required=True,
         help="name or ip-address of host to which to connect"
     )
     parser.add_argument(
-        "username",
-        nargs="?",
+        "-u", "--username", action="store", required=True,
         help="name of user for SSH connection"
     )
-    parser.add_argument(
-        "private_key_file_path",
-        nargs="?",
-        help="patht of private RSA key file to be used for the SSH connection"
+    password_key_group = parser.add_mutually_exclusive_group(required=True)
+    password_key_group.add_argument(
+        "-k", "--private-key-file", action="store", required=False,
+        help="path of private RSA key file to be used for the SSH connection"
+    )
+    password_key_group.add_argument(
+        "-p", "--password", action="store", required=False,
+        help="password of user"
     )
     parser.add_argument(
-        "-d", "--daemon", action="store_true"
+        "-d", "--daemon", action="store_true", help="make server a daemon"
+    )
+    parser.add_argument(
+        "-c", "--client-port", action="store", required=False, default=0, type=int,
+        help="client port for ssh connection (default = 0 (random port))"
+    )
+    parser.add_argument(
+        "-s", "--server-port", action="store", required=False, default=22, type=int,
+        help="server port for ssh connection"
     )
 
     args = parser.parse_args()
@@ -265,7 +308,10 @@ if __name__ == "__main__":
         connection_key=args.connection_key,
         hostname=args.hostname,
         username=args.username,
-        private_key_file_path=args.private_key_file_path
+        password=args.password,
+        private_key_file_path=args.private_key_file,
+        client_port=args.client_port,
+        server_port=args.server_port
     )
 
     ssh_connection.run()
