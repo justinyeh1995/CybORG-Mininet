2282621f253950fe14f449f7f798675d767926f3
justinyeh1995 <justinyeh1995@gmail.com>
2024-04-16 22:08:24 +0000

    print out info so the results bundler can parse it

--- CybORG/Mininet/utils/ssh_action.py
+++ CybORG/Mininet/utils/ssh_action.py
@@ -18,16 +18,23 @@ if __name__ == "__main__":
     parsed_args = parseCmdLineArgs ()
     
     ip = parsed_args.ip
-    sshAction = SshAction(parsed_args.ip, "root", "1234", 22)
+    sshAction = SshAction(ip, "root", "1234", 22)
     #sshAction = SshAction("172.17.0.3", "sftp_user", "art54")
     
     observation = sshAction.execute(None)
-
+    
+    local_socket_info = observation.local_socket_info
+    remote_socket_info = observation.remote_socket_info
     print(observation)
-    print(f"Token: {observation.token}")
     print(f"Available Exploit: {observation.available_exploit}")
-    print(f"Local Socket Info: {observation.local_socket_info}")
-    print(f"Remote Socket Info: {observation.remote_socket_info}")
-    print(f"PID: {observation.pid}")
+    print(f"Local IP: {local_socket_info[0]}")
+    print(f"Local Port: {local_socket_info[1]}")
+    print(f"Remote IP: {remote_socket_info[0]}")
+    print(f"Remote Port: {remote_socket_info[1]}")
+    
+    # print(f"Local Socket Info: {observation.local_socket_info}")
+    # print(f"Remote Socket Info: {observation.remote_socket_info}")
+    # print(f"Token: {observation.token}")
+    # print(f"PID: {observation.pid}")
     
     print("foo")
