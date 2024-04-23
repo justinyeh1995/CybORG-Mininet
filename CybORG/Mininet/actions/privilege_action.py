c28c6c4e0f1e48a7682c874080ad8bca56c470e1
justinyeh1995 <justinyeh1995@gmail.com>
2024-04-23 20:14:38 +0000

    action scripts that use custom actions

---
+++ CybORG/Mininet/actions/privilege_action.py
@@ -0,0 +1,70 @@
+from CybORG.Emulator.Actions.Velociraptor.SSHConnectionServerAction import SSHConnectionServerAction
+from CybORG.Emulator.Actions.Velociraptor.SSHConnectionClientAction import SSHConnectionClientAction
+
+def parseCmdLineArgs ():
+    # parse the command line
+    parser = argparse.ArgumentParser ()
+
+    # add optional arguments
+    parser.add_argument ("-ip", "--ip", default="0.0.0.0", help="IP Address")
+    parser.add_argument ("-host", "--hostname", default="", help="Hostname")
+    parser.add_argument ("-remote", "--remote", default="0.0.0.0", help="Remote IP Address")
+
+    # parse the args
+    args = parser.parse_args ()
+
+    return args
+
+if __name__ == "__main__":
+  
+    parsed_args = parseCmdLineArgs ()
+    
+    ip = parsed_args.ip
+    hostname = parsed_args.hostname
+    remote_ip = parsed_args.remote
+    
+    print(f"At IP Address: {ip}")
+    print(f"Attacker Hostname: {hostname}")
+    print(f"Remote IP Address: {remote_ip}")
+
+    credentials_file = "prog_client.yaml"
+    
+    ssh_connection_server_action = SSHConnectionServerAction(
+        credentials_file=credentials_file,
+        hostname=hostname,
+        remote_hostname=remote_ip,
+        remote_username="root",
+        remote_password="1234",
+        client_port=4444
+    )
+    
+    ssh_connection_server_observation = ssh_connection_server_action.execute(None)
+    
+    ssh_connection_client_action_1 = SSHConnectionClientAction(
+        credentials_file=credentials_file,
+        hostname=hostname,
+        connection_key=ssh_connection_server_observation.connection_key,
+        command="ls -l /etc"
+    )
+    
+    ssh_connection_client_observation_1 = ssh_connection_client_action_1.execute(None)
+    
+    ssh_connection_client_action_2 = SSHConnectionClientAction(
+        credentials_file=credentials_file,
+        hostname=hostname,
+        connection_key=ssh_connection_server_observation.connection_key,
+        command="hostname"
+    )
+    
+    ssh_connection_client_observation_2 = ssh_connection_client_action_2.execute(None)
+    
+    ssh_connection_client_action_3 = SSHConnectionClientAction(
+        credentials_file=credentials_file,
+        hostname=hostname,
+        connection_key=ssh_connection_server_observation.connection_key,
+        command="CLOSE"
+    )
+    
+    ssh_connection_client_observation_3 = ssh_connection_client_action_3.execute(None)
+    
+    print("foo")
---
+++ CybORG/Mininet/actions/ssh_action.py
@@ -0,0 +1,40 @@
+import argparse
+from CybORG.Emulator.Actions.SshAction import SshAction
+
+def parseCmdLineArgs ():
+    # parse the command line
+    parser = argparse.ArgumentParser ()
+
+    # add optional arguments
+    parser.add_argument ("-ip", "--ip", default="0.0.0.0", help="IP Address")
+
+    # parse the args
+    args = parser.parse_args ()
+
+    return args
+
+if __name__ == "__main__":
+  
+    parsed_args = parseCmdLineArgs ()
+    
+    ip = parsed_args.ip
+    sshAction = SshAction(ip, "root", "1234", 22)
+    #sshAction = SshAction("172.17.0.3", "sftp_user", "art54")
+    
+    observation = sshAction.execute(None)
+    
+    local_socket_info = observation.local_socket_info
+    remote_socket_info = observation.remote_socket_info
+    print(observation)
+    print(f"Available Exploit: {observation.available_exploit}")
+    print(f"Local IP: {local_socket_info[0]}")
+    print(f"Local Port: {local_socket_info[1]}")
+    print(f"Remote IP: {remote_socket_info[0]}")
+    print(f"Remote Port: {remote_socket_info[1]}")
+    print(f"PID: {observation.pid}")
+
+    # print(f"Local Socket Info: {observation.local_socket_info}")
+    # print(f"Remote Socket Info: {observation.remote_socket_info}")
+    # print(f"Token: {observation.token}")
+    
+    print("foo")
