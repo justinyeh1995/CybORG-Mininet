a647a393decea102367d2385d46db57a5d7d22c2
justinyeh1995 <justinyeh1995@gmail.com>
2024-07-03 23:30:43 +0000

    Add: return False obs after no host up

--- CybORG/Mininet/mininet_adapter/results_bundler.py
+++ CybORG/Mininet/mininet_adapter/results_bundler.py
@@ -20,6 +20,9 @@ def parse_nmap_network_scan(nmap_output, target, mapper) -> Observation:
     mininet_ip_addresses = re.findall(r'Nmap scan report for (\d+\.\d+\.\d+\.\d+)', nmap_output)
     cyborg_ip_addresses = [mapper.mininet_ip_to_cyborg_ip_map[ip] for ip in mininet_ip_addresses if ip in mapper.mininet_ip_to_cyborg_ip_map]
     
+    if not cyborg_ip_addresses:
+        return Observation(False)
+    
     obs = Observation()
     obs.set_success(True)
     for ip_addr in cyborg_ip_addresses:
