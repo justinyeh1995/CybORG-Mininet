bdc7e6957dfa8530b63f1bb65c0b4410505bed14
justinyeh1995 <justinyeh1995@gmail.com>
2024-07-12 21:33:41 +0000

    Modify mininet_adapter/action_translator.py : Temporary add exploit_remote_service_v2

--- CybORG/Mininet/mininet_adapter/action_translator.py
+++ CybORG/Mininet/mininet_adapter/action_translator.py
@@ -9,7 +9,8 @@ from CybORG.Emulator.Actions.DeployDecoyAction import DeployDecoy
 from pprint import pprint
 
 class ActionTranslator:
-    def __init__(self, path: str, config):
+    def __init__(self, path: str, config, logger):
+        self.logger = logger
         self.path = path
         self.python_exe = config["PYTHON"]["FILE_PATH"]
         self.action_folder_path = self.path + config["ACTION"]["FOLDER_PATH"]
@@ -19,14 +20,20 @@ class ActionTranslator:
 
 
 class RedActionTranslator(ActionTranslator):
-    def __init__(self, path: str, config):
-        super().__init__(path, config)  # Correctly calls the __init__ method of ActionTranslator
+    def __init__(self, path: str, config, logger):
+        super().__init__(path, config, logger)  # Correctly calls the __init__ method of ActionTranslator
         self.action_map = {
             "DiscoverRemoteSystems": self.discover_remote_systems,
             "DiscoverNetworkServices": self.discover_network_services,
-            "ExploitRemoteService": self.exploit_remote_service,
+            "ExploitRemoteService": self.exploit_remote_service_v2,
             "PrivilegeEscalate": self.privilege_escalate
         }
+        self.connection_key={}
+        self.used_ports={}
+        self.exploited_hosts=[]
+        self.priviledged_hosts=[]
+        self.old_exploit_outcome={}
+        self.network_state={}
 
     def translate(self, action_type, target_host, cyborg_to_mininet_host_map, mininet_host_to_ip_map) -> str:
         action_method = self.action_map.get(action_type)
@@ -34,6 +41,7 @@ class RedActionTranslator(ActionTranslator):
             return action_method(target_host, cyborg_to_mininet_host_map, mininet_host_to_ip_map)
         else:
             print("Not Implemented Action...\nSleep for a sec")
+            self.logger.debug(f"Action is {action_method}")
             return "sleep 1"
 
     def discover_remote_systems(self, target_host, cyborg_to_mininet_host_map, mininet_host_to_ip_map):
@@ -53,7 +61,17 @@ class RedActionTranslator(ActionTranslator):
     def exploit_remote_service(self, target_host, cyborg_to_mininet_host_map, mininet_host_to_ip_map):
         print("Red Exploit Network Services")
         host = cyborg_to_mininet_host_map['User0']
+        ## @To-Do Rewirte 
         action = f"{self.python_exe} {self.action_folder_path}/ssh_action.py --ip"
+        target = mininet_host_to_ip_map.get(target_host, cyborg_to_mininet_host_map['User0'])
+        return f'{host} timeout 60 {action} {target}'
+    
+    def exploit_remote_service_v2(self, target_host, cyborg_to_mininet_host_map, mininet_host_to_ip_map):
+        print("Red Exploit Network Services")
+        host = cyborg_to_mininet_host_map['User0']
+        hostname = socket.gethostname()
+        ## @To-Do Rewirte 
+        action = f"{self.python_exe} {self.action_folder_path}/exploit_action.py --hostname {hostname} --remote_hostname"
         target = mininet_host_to_ip_map.get(target_host, cyborg_to_mininet_host_map['User0'])
         return f'{host} timeout 60 {action} {target}'
 
@@ -67,8 +85,8 @@ class RedActionTranslator(ActionTranslator):
 
 
 class BlueActionTranslator(ActionTranslator):
-    def __init__(self, path: str, config):
-        super().__init__(path, config)  # Correctly calls the __init__ method of ActionTranslator
+    def __init__(self, path: str, config, logger):
+        super().__init__(path, config, logger)  # Correctly calls the __init__ method of ActionTranslator
         self.decoy_service_name_to_port = {
             'DecoyApache': 80, 
             'DecoySSHD': 22, 
@@ -82,7 +100,10 @@ class BlueActionTranslator(ActionTranslator):
             "Remove": self.remove,
             "Restore": self.restore,
             "Monitor": self.monitor,
-            "Decoy": self.deploy_decoy
+            "Decoy": self.deploy_decoy,
+            "Analyse": self.analyse,
+            "Sleep": self.sleep,
+            "Monitor": self.monitor,
         }
 
     def translate(self, action_type, target_host, cyborg_to_mininet_host_map, mininet_host_to_ip_map) -> str:
@@ -98,7 +119,11 @@ class BlueActionTranslator(ActionTranslator):
     def remove(self, action_type, target_host, cyborg_to_mininet_host_map, mininet_host_to_ip_map):
         print("Blue Remove")
         # @To-Do Not Implemented as of now
-        return "sleep 1"
+        host = cyborg_to_mininet_host_map['Defender']
+        action = f"{self.python_exe} {self.action_folder_path}/remove.py --ip"
+        target = mininet_host_to_ip_map.get(target_host, cyborg_to_mininet_host_map['User0'])
+        port = self.decoy_service_name_to_port.get(action_type, 80)
+        return f"{host} {action} {target} --port {port}"
 
     def restore(self, action_type, target_host, cyborg_to_mininet_host_map, mininet_host_to_ip_map):
         print("Blue Restore")
@@ -109,6 +134,9 @@ class BlueActionTranslator(ActionTranslator):
         print("Blue Monitor")
         # @To-Do Not Implemented as of now
         return "sleep 1"
+    
+    def sleep(self, action_type, target_host, cyborg_to_mininet_host_map, mininet_host_to_ip_map):
+        return "sleep 1"
 
     def deploy_decoy(self, action_type, target_host, cyborg_to_mininet_host_map, mininet_host_to_ip_map):
         print("Blue Decoy")
@@ -117,3 +145,6 @@ class BlueActionTranslator(ActionTranslator):
         target = mininet_host_to_ip_map.get(target_host, cyborg_to_mininet_host_map['User0'])
         port = self.decoy_service_name_to_port.get(action_type, 80)
         return f"{host} {action} {target} --port {port}"
+    
+    def analyse(self):
+        return "sleep 1"
