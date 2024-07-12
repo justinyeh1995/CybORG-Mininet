4b05bd9e0e7a9a6e515e9a737e026c80d2d7cf05
justinyeh1995 <justinyeh1995@gmail.com>
2024-07-12 21:32:20 +0000

    Modify mininet_adapter/mininet_command_interface.py : update logging

--- CybORG/Mininet/mininet_adapter/mininet_command_interface.py
+++ CybORG/Mininet/mininet_adapter/mininet_command_interface.py
@@ -12,7 +12,7 @@ class MininetCommandInterface:
     
     def start_mininet(self, topology_file: str) -> str:
         try:
-            # self.clean()
+            logging.info ("Starting Mininet network topology")
             path = str(inspect.getfile(CybORG))
             path = path[:-10] + f'/Mininet/mininet_api/custom_net.py' # To-Do: might need to be configurable in the config file
             self.mininet_process = pexpect.spawn(f"sudo python3 {path} -y {topology_file}")
@@ -20,7 +20,7 @@ class MininetCommandInterface:
             self.mininet_process.expect("mininet>")
             return self.mininet_process.before.decode()
         except Exception as e:
-            logging.error(f"Failed to start Mininet: {e}")
+            logging.error (f"Failed to start Mininet: {e}")
             traceback.print_exc()
 
     
@@ -37,7 +37,7 @@ class MininetCommandInterface:
         if self.mininet_process and self.mininet_process.isalive():
             self.mininet_process.sendline('exit')
             self.mininet_process.expect(pexpect.EOF)
-            print("Terminated the ongoing Mininet topology.")
+            logging.info ("Terminated the ongoing Mininet topology.")
 
 
     def clean(self) -> None:
@@ -50,9 +50,9 @@ class MininetCommandInterface:
             cleanup_process = pexpect.spawn("sudo mn -c")
             cleanup_process.timeout = 60
             cleanup_process.expect(pexpect.EOF)  # Wait for the end of the process
-            logging.info("Mininet cleaned up successfully.")
+            logging.info ("Mininet cleaned up successfully.")
         except Exception as e:
-            logging.error(f"Error cleaning up Mininet: {e}")
+            logging.error (f"Error cleaning up Mininet: {e}")
 
         finally:
             if cleanup_process is not None and cleanup_process.isalive():
