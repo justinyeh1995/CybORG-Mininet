5e8b3f61c45fc858f94cd07e42c07176a47a0e3f
justinyeh1995 <justinyeh1995@gmail.com>
2024-07-16 23:24:26 +0000

    Update privilege_action.py - major upgrade

--- CybORG/Mininet/actions/privilege_action.py
+++ CybORG/Mininet/actions/privilege_action.py
@@ -1,7 +1,10 @@
 import argparse
+import inspect
+from CybORG import CybORG
 
 from CybORG.Emulator.Actions.Velociraptor.SSHConnectionServerAction import SSHConnectionServerAction
 from CybORG.Emulator.Actions.Velociraptor.SSHConnectionImpactAction import SSHConnectionImpactAction
+from CybORG.Emulator.Actions.Velociraptor.PrivilegeEscalateActionForMininet import PrivilegeEscalateAction
 from CybORG.Emulator.Observations.Velociraptor.PrivilegeEscalateObservation import PrivilegeEscalateObservation
 
 def parseCmdLineArgs ():
@@ -11,8 +14,10 @@ def parseCmdLineArgs ():
     # add optional arguments
     # parser.add_argument ("-ip", "--ip", default="0.0.0.0", help="IP Address")
     parser.add_argument ("-host", "--hostname", default="cpswtjustin", help="Hostname, use hostname to figure out hostname")
-    parser.add_argument ("-remote", "--remote", default="0.0.0.0", help="Remote IP Address")
-
+    parser.add_argument ("-remote", "--remote_hostname", default="0.0.0.0", help="Remote IP Address")
+    parser.add_argument ("-conn_key", "--conn_key", default="", help="The connection key from the exploit action")
+    parser.add_argument ("-client_port", "--client_port", default=4444, help="The client port used for the ssh session in previous exploit action" )
+    
     # parse the args
     args = parser.parse_args ()
 
@@ -22,43 +27,30 @@ if __name__ == "__main__":
   
     parsed_args = parseCmdLineArgs ()
     
-    # ip = parsed_args.ip
     hostname = parsed_args.hostname
-    remote_ip = parsed_args.remote
+    remote_hostname = parsed_args.remote_hostname    
+    remote_username ="root"
+    remote_password ="1234"
+    conn_key = parsed_args.conn_key
+    client_port = parsed_args.client_port
     
-    # print(f"At IP Address: {ip}")
     print(f"Attacker Hostname: {hostname}")
-    print(f"Remote IP: {remote_ip}")
+    print(f"Remote IP: {remote_hostname}")
+    print(f"Connection Key is: {conn_key}")
 
-    credentials_file = "/home/ubuntu/justinyeh1995/CASTLEGym/CybORG/CybORG/Mininet/actions/prog_client.yaml"  # @To-Do make it configurable
+    path = str(inspect.getfile(CybORG))
+    path = path[:-10]
+    credentials_file = f"{path}/Mininet/actions/prog_client.yaml"  # @To-Do make it configurable
 
-    ssh_connection_server_action = SSHConnectionServerAction(
-        credentials_file=credentials_file,
-        hostname=hostname,
-        remote_hostname=remote_ip,
-        remote_username="root", # 'velociraptor'
-        remote_password="1234",
-        client_port=4444
-    )
-    
-    ssh_connection_server_observation = ssh_connection_server_action.execute(None)
-    
-    ssh_connection_client_action_gain_root_access = SSHConnectionImpactAction(
-        credentials_file=credentials_file,
-        hostname=hostname,
-        connection_key=ssh_connection_server_observation.connection_key,
-        command="sudo su -"
-    )
-    
-    ssh_connection_client_observation_2 = ssh_connection_client_action_gain_root_access.execute(None)
-    
-    ssh_connection_client_action_3 = SSHConnectionImpactAction(
-        credentials_file=credentials_file,
-        hostname=hostname,
-        connection_key=ssh_connection_server_observation.connection_key,
-        command="CLOSE"
-    )
-    
-    # ssh_connection_client_observation_3 = ssh_connection_client_action_3.execute(None)
-    print(ssh_connection_client_observation_2.data)
-    print("foo")
+    pes_action=PrivilegeEscalateAction(credentials_file,hostname,conn_key,remote_hostname,remote_username,remote_password,client_port)
+
+    observation=pes_action.execute(None)
+    print("Success is:",observation.success)
+    print("Current User?:",observation.user)
+    print("Any new host explored?:",observation.explored_host)
+    print("PID of malicious process?",observation.pid)
+    print('!!Please clean the mess after test!!')
+
+    print('!! Cleaning mess, just for this testing, in real action cleaning need to be done by Blue Agent!!')
+    cleaned= pes_action.run_command("CLOSE")
+    print('!!cleaned and connection',cleaned,'!!')
