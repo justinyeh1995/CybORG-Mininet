a0efd555952f28fd39dfd4a1a46136ce2513f755
justinyeh1995 <justinyeh1995@gmail.com>
2024-07-22 21:17:58 +0000

    Upgade entity.py - remove possiblity of circular import

--- CybORG/Mininet/mininet_adapter/entity.py
+++ CybORG/Mininet/mininet_adapter/entity.py
@@ -1,15 +1,17 @@
 from dataclasses import dataclass, field
 from typing import Any
 
-from CybORG.Mininet import MininetAdapter
+from CybORG.Mininet import MininetAdapter #This can cause circular import, but it is fine as of now
 
 @dataclass
 class Entity:
     """_summary_
-    
+    A class that is used to represent an entity in the Mininet Adapter
+    mainly used for the purpose of upcalling the Mininet Adapter from the sub-classes
     """
     isRegister: bool = False
     mininet_adpator: MininetAdapter = None
+    
     def register(self, mininet_adpator):
         self.mininet_adpator = mininet_adpator # For Upper level access, upcalling purpose
         self.isRegister = True
