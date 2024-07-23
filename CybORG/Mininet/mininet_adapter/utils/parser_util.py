c507657f9c10434ff2c6870d49af3c56ab4aaede
justinyeh1995 <justinyeh1995@gmail.com>
2024-07-23 20:47:09 +0000

    Refactor util.py and make three XX_util.py - mapper_util.py / parser_util.py / yaml_topo_util.py

---
+++ CybORG/Mininet/mininet_adapter/utils/parser_util.py
@@ -0,0 +1,262 @@
+import re
+import traceback 
+from pprint import pprint
+from typing import List, Dict, Iterator, Pattern
+from ipaddress import IPv4Address, IPv4Network
+
+from CybORG.Shared import Observation
+from CybORG.Mininet.mininet_adapter.entity import Entity
+
+
+def find_matches(pattern_str: str, raw_text: str) -> Iterator[re.Match]:
+    # Define a regex pattern to extract hostnames and IP addresses
+    pattern: Pattern = re.compile(pattern_str)
+    
+    # Find all matches in the decoded output
+    match: Iterator[re.Match] = pattern.finditer(raw_text)
+    
+    return match
+
+def enum_to_boolean(enum_value):
+    if enum_value == 'TRUE':
+        return True
+    elif enum_value == 'FALSE':
+        return False
+    else:
+        return None
+
+def parse_nmap_network_scan(nmap_output, target, mapper) -> Observation:
+    subnet = target
+    mininet_ip_addresses = re.findall(r'Nmap scan report for (\d+\.\d+\.\d+\.\d+)', nmap_output)
+    cyborg_ip_addresses = [mapper.mininet_ip_to_cyborg_ip_map[ip] for ip in mininet_ip_addresses if ip in mapper.mininet_ip_to_cyborg_ip_map]
+    
+    if not cyborg_ip_addresses:
+        return Observation(False)
+    
+    obs = Observation()
+    obs.set_success(True)
+    for ip_addr in cyborg_ip_addresses:
+        hostid = mapper.cyborg_ip_to_host_map[str(ip_addr)]
+        if "router" in hostid:
+            continue 
+        obs.add_interface_info(hostid=hostid, ip_address=ip_addr, subnet=subnet)
+    return obs
+    
+def parse_nmap_port_scan(nmap_output, target, mapper) -> List:
+    res = {'success': True}
+    mininet_host = target
+    ip = mapper.mininet_host_to_cyborg_ip_map[mininet_host]
+
+    # Regular expression to match the port information
+    port_info_regex = re.compile(r'(\d+)/(\w+)\s+open\s+(\w+)\s+(.+)')
+    
+    # Find all matches
+    matches = port_info_regex.findall(nmap_output)
+    
+    # Process matches
+    processes = []
+    for match in matches:
+        port_number, protocol, service_name, version = match
+        processes.append({
+            'port': port_number,
+            'protocol': protocol,
+            'service': service_name,
+            'version': version.strip()
+        })    
+
+    obs = Observation()
+    obs.set_success(True)
+    for proc in processes:
+        hostid = mapper.cyborg_ip_to_host_map[str(ip)]
+        obs.add_process(hostid=hostid, local_port=proc["port"], local_address=ip)
+        
+    return obs
+
+def parse_exploit_action(ssh_action_output, mapper) -> Observation:
+    ansi_escape = re.compile(r'\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])')
+    ssh_action_output = ansi_escape.sub('', ssh_action_output)
+    
+    pattern = re.compile(r'TRUE|FALSE')
+
+    # Use re.search to find a match
+    match = pattern.search(ssh_action_output)
+    
+    # Extract the 'TRUE' or 'FALSE' part if found
+    success_status = enum_to_boolean(match.group()) if match else None
+    
+    obs = Observation(success_status)
+    
+    if not success_status:
+        #@To-Do: Follow wrapper branch?? 
+        return obs
+    
+    # print(ssh_action_output)
+
+    pattern = r"(\d+\.\d+\.\d+\.\d+):(\d+)\s+(\d+\.\d+\.\d+\.\d+):(\d+).*pid=(\d+)"
+    
+    # Use re.findall() to extract the values
+    match = re.search(pattern, ssh_action_output)
+
+    pattern = r"Connection Key is: ([a-zA-Z0-9]*)" # parse connection key
+    match_key = re.search(pattern, ssh_action_output)
+    conn_key = match_key.group(1) # .groups() returns a tuple, group(1) group(0): Connection Key is: 1C01IZBLD3 group(1): 1C01IZBLD3 group(): Connection Key is: 1C01IZBLD3
+    
+    data = {}
+    if match:
+        # local_ip, port, remote_ip, port_for_reverse_shell, pid = match.groups()
+        attacked_ip, attacked_port, attacker_ip, attacker_port, pid = match.groups()
+        
+        attacked_host_name = mapper.cyborg_ip_to_host_map.get(attacked_ip)
+        
+        attacker_node = attacker_ip #mapper.cyborg_host_to_ip_map.get('User0')
+        
+        attack_start_ip= 21
+        
+        data[attacked_ip] = {
+            "Processes": [{
+                "Connections": [{
+                    "local_port": attacked_port,
+                    "remote_port": attacker_port,
+                    "local_address": IPv4Address(attacked_ip),
+                    "remote_address": IPv4Address(attacker_ip)
+                }],
+                "Process Type": 'ProcessType.REVERSE_SESSION'
+            }, 
+            {
+                "Connections": [{
+                    "local_port": attacked_port,
+                    "local_address": IPv4Address(attacked_ip),
+                    "Status": 'ProcessState.OPEN'
+                }],
+                "Process Type": 'ProcessType.FEMITTER' #??
+            }],
+            "Interface": [{
+                "IP Address": IPv4Address(attacked_ip)
+            }],
+            "Sessions": [{
+                "ID": 1,
+                'Username':'SYSTEM',
+                "Type": 'SessionType.RED_REVERSE_SHELL',
+                "Agent": "Red"
+            }],
+            "System info": {
+                "Hostname": attacked_host_name,
+                "OSType": 'OperatingSystemType.LINUX'
+            }
+        }
+            
+        data[attacker_node]={
+            "Processes": [{
+                "Connections": [{
+                    "local_port": attacker_port,
+                    "remote_port": attacked_port,
+                    "local_address": IPv4Address(attacker_ip),
+                    "remote_address": IPv4Address(attacked_ip)
+                }],
+                "Process Type": 'ProcessType.REVERSE_SESSION_HANDLER'
+            }],
+            "Interface": [{
+                "IP Address": IPv4Address(attacker_ip)
+            }]
+        }
+        
+        data["Additional Info"]= { 
+            "Connection Key": conn_key,
+            "Attacked IP": attacked_ip,
+            "Attacker Port": attacker_port,
+        }        
+
+    obs.data.update(data)
+    
+    return obs
+
+def parse_escalate_action(escalate_action_output, mapper) -> Observation:
+    ansi_escape = re.compile(r'\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])')
+    escalate_action_output = ansi_escape.sub('', escalate_action_output)
+    
+    pattern = re.compile(r'TRUE|FALSE')
+
+    # Use re.search to find a match
+    match = pattern.search(escalate_action_output)
+    
+    # Extract the 'TRUE' or 'FALSE' part if found
+    success_status = enum_to_boolean(match.group()) if match else None
+    
+    obs = Observation(success_status)
+    
+    if not success_status:
+        return obs
+    
+    # Parse Escalated Host IP
+    pattern1 = r"Remote IP: (\d+\.\d+\.\d+\.\d+)"
+    
+    # Use re.findall() to extract the values
+    matches = re.findall(pattern1, escalate_action_output)
+    
+    pattern_explored = r"Any new host explored\?: (\d+\.\d+\.\d+\.\d+)"
+    match_explored = re.search(pattern_explored, escalate_action_output)
+    explored_host_ip = match_explored.group(1) if match_explored else None
+        
+    data = {}
+    if matches:
+        remote_ip = matches[0]
+        subnet_cidr = mapper.cyborg_ip_to_subnet[remote_ip]
+        remote_hostname = mapper.cyborg_ip_to_host_map[remote_ip]
+        
+        if explored_host_ip:    
+            explored_host_name = mapper.cyborg_ip_to_host_map.get(explored_host_ip, "")
+            data[explored_host_name] = {     
+                'Interface': [{'IP Address': IPv4Address(explored_host_ip)}]
+            }
+
+        data[remote_hostname] = {'Interface': [{'IP Address': IPv4Address(remote_ip),
+                            'Interface Name': 'eth0',
+                            'Subnet': IPv4Network(subnet_cidr)}],
+                    'Sessions': [{'Agent': 'Red',
+                            'ID': 1,
+                            'Type': 'SessionType.SSH: 2',
+                            'Username': 'root'}],
+                    'System info': {'Hostname': remote_hostname, 'OSType': 'LINUX'}
+        }
+    '''
+    Red Action: PrivilegeEscalate User1
+    ----------------------------------------------------------------------------
+    data = {'Enterprise1': {'Interface': [{'IP Address': IPv4Address('10.0.74.170')}]},
+            'User1': {'Interface': [{'IP Address': IPv4Address('10.0.103.205'),
+                            'Interface Name': 'eth0',
+                            'Subnet': IPv4Network('10.0.103.192/28')}],
+                    'Sessions': [{'Agent': 'Red',
+                            'ID': 1,
+                            'Type': <SessionType.SSH: 2>,
+                            'Username': 'SYSTEM'}]},
+            'success': <TrinaryEnum.TRUE: 1>}
+    '''
+    obs.data.update(data)
+    return obs
+    
+def parse_remove_action(remove_action_string: str) -> Observation:
+    pattern = re.compile(r'TRUE|FALSE')
+
+    # Use re.search to find a match
+    match = pattern.search(remove_action_string)
+    
+    # Extract the 'TRUE' or 'FALSE' part if found
+    success_status = enum_to_boolean(match.group()) if match else None
+    
+    obs = Observation(success_status)
+    
+    return obs
+
+def parse_decoy_action(decoy_action_output) -> Observation:
+    pattern = re.compile(r'TRUE|FALSE')
+
+    # Use re.search to find a match
+    match = pattern.search(decoy_action_output)
+    
+    # Extract the 'TRUE' or 'FALSE' part if found
+    success_status = enum_to_boolean(match.group()) if match else None
+    
+    # print(f"Match is: {match} \n")
+    
+    return Observation(success_status)
+    
--- CybORG/Mininet/mininet_adapter/utils/yaml_topo_util.py
+++ CybORG/Mininet/mininet_adapter/utils/yaml_topo_util.py
@@ -1,59 +1,7 @@
-import re
-import collections
-from pprint import pprint
-from typing import Iterator, Pattern, Tuple, Dict, List
+from typing import List
 import ipaddress
-from ipaddress import IPv4Address, IPv4Network
+from ipaddress import IPv4Network
 from CybORG.Mininet.mininet_api import custom_utils as cu
-
-###########################
-# CybORG-2-Mininet Mapper #
-###########################
-
-def build_cyborg_to_mininet_name_map(cyborg) -> Dict:
-    cyborg_to_mininet_name_map = collections.defaultdict(str)
-    
-    for cnt, (lan_name, network) in enumerate(cyborg.environment_controller.subnet_cidr_map.items()):
-            cyborg_to_mininet_name_map[lan_name] = f'lan{cnt+1}'
-            cyborg_to_mininet_name_map[f'{lan_name}_router'] = f'r{cnt+1}'
-        
-    return cyborg_to_mininet_name_map
-
-
-def parse_mininet_ip(raw_text: str) -> list:
-    pattern_str1: str = r'(Router):\s+(?P<host>\S+)\s+with IP:\s+(?P<ip>\S+)'
-    pattern_str2: str = r'(Host)\s+(?P<host>\S+)\s+with IP:\s+(?P<ip>\S+)'
-    pattern_str3: str = r'(NAT)\s+(?P<host>\S+) with IP: (?P<ip>\S+)'
-
-    # Find all matches in the decoded output
-    matches1: Iterator[re.Match] = find_matches(pattern_str1, raw_text)
-    matches2: Iterator[re.Match] = find_matches(pattern_str2, raw_text)
-    matches3: Iterator[re.Match] = find_matches(pattern_str3, raw_text)
-    # Convert match iterators to lists and concatenate them
-    matches: list = list(matches1) + list(matches2) + list(matches3)
-
-    return matches
-
-
-def build_mininet_host_to_cyborg_ip_map(topology):
-    mininet_host_to_cyborg_ip_map = {}
-    for entry in topology["topo"]['lans']:
-        lan_name = entry['name']
-        for host, ip in entry['hosts_info'].items():
-            mininet_host_to_cyborg_ip_map[f"{lan_name}{host}"] = ip
-        mininet_host_to_cyborg_ip_map[entry['router']] = entry['router_ip']
-    return mininet_host_to_cyborg_ip_map
-
-
-def build_cyborg_ip_to_mininet_host_map(topology):
-    cyborg_ip_to_mininet_host_map = {}
-    for entry in topology["topo"]['lans']:
-        lan_name = entry['name']
-        for host, ip in entry['hosts_info'].items():
-            cyborg_ip_to_mininet_host_map[ip] = f"{lan_name}{host}"
-        cyborg_ip_to_mininet_host_map[entry['router_ip']] = entry['router']
-    return cyborg_ip_to_mininet_host_map
-    
 
 ################
 # yaml builder #
@@ -211,16 +159,3 @@ def generate_routing_rules(topology):
             router_rules["entries"].append(entry_str)
 
     return routing_rules
-
-###################
-# Results Bundler #
-###################
-
-def find_matches(pattern_str: str, raw_text: str) -> Iterator[re.Match]:
-    # Define a regex pattern to extract hostnames and IP addresses
-    pattern: Pattern = re.compile(pattern_str)
-    
-    # Find all matches in the decoded output
-    match: Iterator[re.Match] = pattern.finditer(raw_text)
-    
-    return match
