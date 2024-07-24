46da59f57003399c5b51adabd4b8bb012a2c0cff
justinyeh1995 <justinyeh1995@gmail.com>
2024-07-24 22:25:06 +0000

    Update actions/deploy_decoy_action.py : log out decoyname and cyborg hostname so that decoy observation will consist of this info

--- CybORG/Mininet/actions/deploy_decoy_action.py
+++ CybORG/Mininet/actions/deploy_decoy_action.py
@@ -7,7 +7,8 @@ def parseCmdLineArgs ():
 
     # add optional arguments
     parser.add_argument ("-ip", "--ip", default="0.0.0.0", help="IP Address")
-
+    parser.add_argument ("-cyborg_hostname", "--cyborg_hostname", default="User3", help="Hostname")
+    parser.add_argument ("-decoyname", "--decoyname", default='Apache', type=str, help="Decoy services")
     parser.add_argument ("-port", "--port", default=80, type=int, help="Port Number")
 
 
@@ -20,10 +21,11 @@ if __name__ == "__main__":
 
     parsed_args = parseCmdLineArgs ()
     
-    deploy_decoy = DeployDecoy(parsed_args.ip, 'root', '1234', 'apache', parsed_args.port)
+    deploy_decoy = DeployDecoy(parsed_args.ip, 'root', '1234', parsed_args.decoyname, parsed_args.port)
     
     observation = deploy_decoy.execute(None)
 
     print(observation)
-
-    print("foo")
+    print("Decoied Service:", parsed_args.decoyname)
+    print("Decoy Deployed IP:", parsed_args.ip)
+    print("Decoy Deployed Hostname:", parsed_args.cyborg_hostname)
