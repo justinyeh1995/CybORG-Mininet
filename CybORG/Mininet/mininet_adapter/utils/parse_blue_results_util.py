2d98ede85e38b2a9f4c87b4b4505a0a2d3948449
justinyeh1995 <justinyeh1995@gmail.com>
2024-07-24 22:23:58 +0000

    Update utils/parse_blue_results_util.py : adpoted transform_decoy from wrapper branch to enhance decoy obs

--- CybORG/Mininet/mininet_adapter/utils/parse_blue_results_util.py
+++ CybORG/Mininet/mininet_adapter/utils/parse_blue_results_util.py
@@ -1,5 +1,6 @@
 import re
 import traceback 
+import random
 from typing import List, Dict, Iterator, Pattern
 from ipaddress import IPv4Address, IPv4Network
 
@@ -19,7 +20,7 @@ def parse_remove_action(remove_action_string: str) -> Observation:
     
     return obs
 
-def parse_decoy_action(decoy_action_output) -> Observation:
+def parse_decoy_action(decoy_action_output: str) -> Observation:
     pattern = re.compile(r'TRUE|FALSE')
 
     # Use re.search to find a match
@@ -28,6 +29,54 @@ def parse_decoy_action(decoy_action_output) -> Observation:
     # Extract the 'TRUE' or 'FALSE' part if found
     success_status = enum_to_boolean(match.group()) if match else None
     
-    # print(f"Match is: {match} \n")
+    obs = Observation(success_status)
     
-    return Observation(success_status)
+    pattern_decoyname = r"Decoied Service:\s+(\w+)"
+    match_decoyname = re.search(pattern_decoyname, decoy_action_output)
+    decoyname = match_decoyname.group(1) if match_decoyname else None
+    
+    pattern_host = r"Decoy Deployed Hostname:\s+(\w+)"
+    match_host = re.search(pattern_host, decoy_action_output)
+    host = match_host.group(1) if match_host else None
+    
+    # print(f"Match is: {match} \n")
+    data = {'host': host,
+        'username': 'root',
+        'decoyname': decoyname,
+        }
+    formatted_data = transform_decoy(data)
+    obs.data.update(formatted_data)
+    
+    return obs
+
+
+def transform_decoy(data):
+    # TO do : Remove PID and PPID as fixed to fetch from process and update 
+    #       : If possible remove the propertiesa and set it during the decoy set up to lure/honeytrap. 
+    #       : Remove the faked decoys that is not part of linux type decoys. 
+    #print('Data is:',data)
+    formatted_data= {}
+    host=data['host']
+    username=data['username']
+    decoyname=data['decoyname']
+
+    decoyname= decoyname.lower()
+    formatted_data[host] = {
+            'Processes': [
+                {
+                    'PID': random.randint(1000,5000),  # Static PID since it's not provided in the input
+                    'PPID': 1,                # Static PPID since it's not provided in the input
+                    'Service Name': decoyname,  # Assuming a static service name; replace if variable
+                    'Username': username
+                }
+            ]
+        }
+
+    #Decoy specific modification
+    if decoyname=='tomcat':
+        formatted_data[host]['Processes'][0]['Properties']=['rfi']
+    elif decoyname=='femitter':
+        formatted_data[host]['Processes'][0]['Username']='SYSTEM'
+    elif decoyname=='harakasmpt': 
+        formatted_data[host]['Processes'][0]['Service Name']='haraka'
+    return formatted_data
