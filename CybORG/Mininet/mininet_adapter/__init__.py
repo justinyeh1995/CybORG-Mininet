611b139093d6a88041d2099d5c8b3d8f035953b7
justinyeh1995 <justinyeh1995@gmail.com>
2024-07-26 21:32:46 +0000

    Modify mininet_adapter/__init__.py to involve ActionTraslator class as well

--- CybORG/Mininet/mininet_adapter/__init__.py
+++ CybORG/Mininet/mininet_adapter/__init__.py
@@ -1,7 +1,7 @@
 from .yaml_topology_manager import YamlTopologyManager
 from .mininet_command_interface import MininetCommandInterface
 from .cyborg_mininet_mapper import CybORGMininetMapper
-from .action_translator import RedActionTranslator, BlueActionTranslator
+from .action_translator import RedActionTranslator, BlueActionTranslator, ActionTranslator
 from .results_bundler import ResultsBundler
 from .reward_calculator import RewardCalculator
 from .topology_asset_manager import TopologyAssetManager
