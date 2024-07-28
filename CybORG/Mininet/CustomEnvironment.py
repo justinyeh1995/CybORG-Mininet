53369c974f7f8e4dd00a6e549e8308f26cc4ff46
justinyeh1995 <justinyeh1995@gmail.com>
2024-07-27 19:56:28 -0500

    Modify CustomEnvironment.py : game state manager and mininet adapter now lives entirely here

--- CybORG/Mininet/CustomEnvironment.py
+++ CybORG/Mininet/CustomEnvironment.py
@@ -7,9 +7,11 @@ from CybORG import CybORG, CYBORG_VERSION
 
 from CybORG.Shared import Observation
 
+from CybORG.GameVisualizer.GameStateCollector import GameStateCollector
+from CybORG.Mininet.MininetAdapter import MininetAdapter
 
 class CybORGEnvironment(ABC):
-    def __init__(self, cyborg, red_agent, blue_agent, num_steps, max_episode):
+    def __init__(self, cyborg, red_agent, blue_agent, num_steps, max_episode, environment):
         self.cyborg = cyborg
         self.red_agent = red_agent
         self.blue_agent = blue_agent
@@ -26,21 +28,21 @@ class CybORGEnvironment(ABC):
         self.blue_table_cyborg = cyborg.env.env.env.env
         self.true_table_cyborg = cyborg.env.env.env.env.env
         self.unwrapped_cyborg = cyborg.env.env.env.env.env.env
+        
+        self.game_state_manager =  GameStateCollector(environment=environment)
+        # Set up game_state_manager
+        self.game_state_manager.set_environment(cyborg=self.unwrapped_cyborg,
+                                            red_agent_name=self.red_agent_name,
+                                            blue_agent_name=self.blue_agent_name,
+                                            num_steps=num_steps)
 
     @abstractmethod
     def run(self):
         pass
 
 class SimulatedEnvironment(CybORGEnvironment):
-    def __init__(self, cyborg, red_agent, blue_agent, num_steps, max_episode, game_state_manager):
-        super().__init__(cyborg, red_agent, blue_agent, num_steps, max_episode)
-        self.game_state_manager = game_state_manager
-
-        # Set up game_state_manager
-        self.game_state_manager.set_environment(cyborg=self.unwrapped_cyborg,
-                                            red_agent_name=self.red_agent_name,
-                                            blue_agent_name=self.blue_agent_name,
-                                            num_steps=num_steps)
+    def __init__(self, cyborg, red_agent, blue_agent, num_steps, max_episode, environment):
+        super().__init__(cyborg, red_agent, blue_agent, num_steps, max_episode, environment)
 
     def run(self):
         blue_observation = self.cyborg.reset()
@@ -86,100 +88,91 @@ class SimulatedEnvironment(CybORGEnvironment):
         return total_reward, actions_list
 
 class EmulatedEnvironment(CybORGEnvironment):
-    def __init__(self, cyborg, red_agent, blue_agent, num_steps, max_episode, game_state_manager, mininet_adapter):
-        super().__init__(cyborg, red_agent, blue_agent, num_steps, max_episode)
-        self.game_state_manager = game_state_manager
-
-        # Set up game_state_manager
-        self.game_state_manager.set_environment(cyborg=self.unwrapped_cyborg,
-                                            red_agent_name=self.red_agent_name,
-                                            blue_agent_name=self.blue_agent_name,
-                                            num_steps=num_steps)
-        # Set up mininet_adapter
-        self.mininet_adapter = mininet_adapter
-        self.mininet_adapter.set_environment(cyborg=self.unwrapped_cyborg)
-
+    """_summary_
+        This is where CybORG interacts with the MininetAdapter to run the environment
+    Args:
+        CybORGEnvironment (_type_): The parent class
+    """
+    def __init__(self, cyborg, red_agent, blue_agent, num_steps, max_episode, environment):
+        super().__init__(cyborg, red_agent, blue_agent, num_steps, max_episode, environment)
 
     def run(self):
         actions_list = []
         total_reward = []
+        
+        with MininetAdapter() as mininet_adapter:
+            # Set up mininet_adapter
+            self.mininet_adapter = mininet_adapter
+            self.mininet_adapter.set_environment(cyborg=self.unwrapped_cyborg)
 
-        for i in range(self.max_episode):
-            r = []
-            a = []
+            for i in range(self.max_episode):
+                r = []
+                a = []
 
-            blue_observation = self.linked_diagram_cyborg.reset() # @To-Do; Follow wrapper branch and use the obs from assets/blue_init_observation.json
-            blue_action_space = self.challenge_wrapper.get_action_space('Blue')
-            red_observation = self.true_table_cyborg.get_observation('Red')
-            red_action_space = self.true_table_cyborg.get_action_space('Red')
+                blue_observation = self.linked_diagram_cyborg.reset() # @To-Do; Follow wrapper branch and use the obs from assets/blue_init_observation.json
+                blue_action_space = self.challenge_wrapper.get_action_space('Blue')
+                red_observation = self.true_table_cyborg.get_observation('Red')
+                red_action_space = self.true_table_cyborg.get_action_space('Red')
 
-            mininet_blue_observation = blue_observation
-            mininet_red_observation = red_observation
+                mininet_blue_observation = blue_observation
+                mininet_red_observation = red_observation
 
 
-            # @To-Do bad design, the reset() among different objects has ordering dependecies
-            try:
-                self.game_state_manager.reset()
-            except Exception as e:
-                traceback.print_exc()
-                raise e
-            
-            try:
-                self.mininet_adapter.reset()
-            except Exception as e:
-                traceback.print_exc()
-                raise e
-            
-            # try:
-            #     self.mininet_adapter.topology_asset_manager.reset()
-            #     # assert type(self.mininet_adapter.topology_asset_manager.get_inti_blue_obs()) == type(mininet_blue_observation) # needs type checking 
-            #     mininet_blue_observation = self.mininet_adapter.topology_asset_manager.get_inti_blue_obs()
-            # except Exception as e:
-            #     traceback.print_exc()
-            #     raise e
-            
-            for j in range(self.num_steps):
-                red_observation = mininet_red_observation.data if isinstance(mininet_red_observation, Observation) else mininet_red_observation
-                red_action = self.red_agent.get_action(red_observation, red_action_space)
-                print("--> In main loop: Red action using mininet_observation")
-                print(red_action)
+                # @To-Do bad design, the reset() among different objects has ordering dependecies
+                try:
+                    self.game_state_manager.reset()
+                except Exception as e:
+                    # traceback.print_exc()
+                    raise e
                 
                 try:
-                    mininet_red_observation, red_reward = self.mininet_adapter.step(str(red_action), agent_type='Red')
-
+                    mininet_adapter.reset()
                 except Exception as e:
-                    traceback.print_exc()
+                    # traceback.print_exc()
                     raise e
+                
+                for j in range(self.num_steps):
+                    red_observation = mininet_red_observation.data if isinstance(mininet_red_observation, Observation) else mininet_red_observation
+                    red_action = self.red_agent.get_action(red_observation, red_action_space)
+                    print("--> In main loop: Red action using mininet_observation")
+                    print(red_action)
+                    
+                    try:
+                        mininet_red_observation, red_reward = mininet_adapter.step(str(red_action), agent_type='Red')
 
-                blue_observation = mininet_blue_observation
-                # blue_observation = self.blue_table_cyborg.observation_change('Blue', blue_observation.data) if isinstance(blue_observation, Observation) else blue_observation
-                blue_observation = self.blue_table_cyborg.observation_change(blue_observation.data) if isinstance(blue_observation, Observation) else blue_observation
-                blue_observation = np.array(blue_observation, dtype=np.float32)
+                    except Exception as e:
+                        # traceback.print_exc()
+                        raise e
 
-                blue_action = self.blue_agent.get_action(blue_observation, blue_action_space)
-                # blue_possible_actions = self.openai_gym_cyborg.possible_actions[blue_action]
-                blue_possible_actions = self.openai_gym_cyborg.get_attr('possible_actions')[blue_action]
-                print("--> In main loop: Blue action using mininet_observation")
-                print(blue_possible_actions)
+                    blue_observation = mininet_blue_observation
+                    # blue_observation = self.blue_table_cyborg.observation_change('Blue', blue_observation.data) if isinstance(blue_observation, Observation) else blue_observation
+                    blue_observation = self.blue_table_cyborg.observation_change(blue_observation.data) if isinstance(blue_observation, Observation) else blue_observation
+                    blue_observation = np.array(blue_observation, dtype=np.float32)
 
-                mininet_blue_observation, blue_reward = self.mininet_adapter.step(str(blue_possible_actions), agent_type='Blue')
+                    blue_action = self.blue_agent.get_action(blue_observation, blue_action_space)
+                    # blue_possible_actions = self.openai_gym_cyborg.possible_actions[blue_action]
+                    blue_possible_actions = self.openai_gym_cyborg.get_attr('possible_actions')[blue_action]
+                    print("--> In main loop: Blue action using mininet_observation")
+                    print(blue_possible_actions)
 
-                actions = {"Red": str(red_action), "Blue": str(blue_possible_actions)}
-                observations = {"Red": mininet_red_observation.data, "Blue": mininet_blue_observation.data}
-                rewards = {"Red": red_reward[-1] + blue_reward[-1], "Blue": red_reward[0] + blue_reward[0]}
+                    mininet_blue_observation, blue_reward = mininet_adapter.step(str(blue_possible_actions), agent_type='Blue')
 
-                state_snapshot = self.game_state_manager.create_state_snapshot(actions, observations, rewards)
-                self.game_state_manager.store_state(state_snapshot, i, j)
-                r.append(rewards['Blue'])
-                a.append((actions['Blue'], actions['Red']))
+                    actions = {"Red": str(red_action), "Blue": str(blue_possible_actions)}
+                    observations = {"Red": mininet_red_observation.data, "Blue": mininet_blue_observation.data}
+                    rewards = {"Red": red_reward[-1] + blue_reward[-1], "Blue": red_reward[0] + blue_reward[0]}
 
-                print(f"===Episode {i+1}, Round {j + 1} is over===")
+                    state_snapshot = self.game_state_manager.create_state_snapshot(actions, observations, rewards)
+                    self.game_state_manager.store_state(state_snapshot, i, j)
+                    r.append(rewards['Blue'])
+                    a.append((actions['Blue'], actions['Red']))
 
-            self.blue_agent.end_episode()
-            self.red_agent.end_episode()
-            total_reward.append(sum(r))
-            actions_list.append(a)
+                    print(f"===Episode {i+1}, Round {j + 1} is over===")
 
-        self.mininet_adapter.clean()
+                self.blue_agent.end_episode()
+                self.red_agent.end_episode()
+                total_reward.append(sum(r))
+                actions_list.append(a)
+
+            # mininet_adapter.clean()
 
         return total_reward, actions_list
