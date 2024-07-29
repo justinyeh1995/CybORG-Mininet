9fad74b73ab9eba27b5b258af8c57407ce3437bd
justinyeh1995 <justinyeh1995@gmail.com>
2024-07-29 18:15:33 -0500

    Update mininet_adapter/utils/parse_blue_results_util.py : add parse_analyse_action

--- CybORG/Mininet/mininet_adapter/utils/parse_blue_results_util.py
+++ CybORG/Mininet/mininet_adapter/utils/parse_blue_results_util.py
@@ -98,3 +98,19 @@ def parse_reset_action(reset_action_output: str) -> Observation:
     else:
         return Observation(False)
     
+    
+def parse_analyse_action(analyse_action_output: str) -> Observation:
+    '''
+    md5 chksum are: {'/tmp/lan1h1/ubuntu/file6.txt': '2ed1da9e5db09fa95f42eacf0f203c88', '/tmp/lan1h1/ubuntu/file0.txt': '55bb703ba889096db15b539a1141c109', '/tmp/lan1h1/ubuntu/file5.txt': '36a9d8ed8066b9094ff73ac221d585fb', '/tmp/lan1h1/ubuntu/file1.txt': 'f4bdbfe06a7dbf5aaaa71ddd625f9881', '/tmp/lan1h1/ubuntu/file4.txt': '60f46204641696f89c34ca6c3a665131', '/tmp/lan1h1/ubuntu/file2.txt': '0ede6689285a69a30ec1e5131b3f721a', '/tmp/lan1h1/ubuntu/file3.txt': '0556da718ead47d7e4e178d80f0ff1cf'}
+    turn this string into a dictionary
+    '''
+    test = "md5 chksum are: {'/tmp/lan1h1/ubuntu/file6.txt': '2ed1da9e5db09fa95f42eacf0f203c88', '/tmp/lan1h1/ubuntu/file0.txt': '55bb703ba889096db15b539a1141c109', '/tmp/lan1h1/ubuntu/file5.txt': '36a9d8ed8066b9094ff73ac221d585fb', '/tmp/lan1h1/ubuntu/file1.txt': 'f4bdbfe06a7dbf5aaaa71ddd625f9881', '/tmp/lan1h1/ubuntu/file4.txt': '60f46204641696f89c34ca6c3a665131', '/tmp/lan1h1/ubuntu/file2.txt': '0ede6689285a69a30ec1e5131b3f721a', '/tmp/lan1h1/ubuntu/file3.txt': '0556da718ead47d7e4e178d80f0ff1cf'}"
+    pattern = r"md5 chksum are: ({.*})"
+    match = re.search(pattern, analyse_action_output)
+    if match:
+        md5_dict: Dict = eval(match.group(1))
+        obs = Observation(True)
+        obs.data.update({"MD5": md5_dict})
+        return obs
+    else:
+        return Observation(False)
