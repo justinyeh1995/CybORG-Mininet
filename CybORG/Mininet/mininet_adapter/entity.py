fa4de485657087a5939a5bd32c023237289cbe46
justinyeh1995 <justinyeh1995@gmail.com>
2024-07-30 20:04:42 -0500

    Debug mininet_adapter/entity.py : Importing MininetAdapter for type references invoke circular dependecies

--- CybORG/Mininet/mininet_adapter/entity.py
+++ CybORG/Mininet/mininet_adapter/entity.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass, field
 from typing import Any
 
-from CybORG.Mininet import MininetAdapter #This can cause circular import, but it is fine as of now
+# from CybORG.Mininet import MininetAdapter #This can cause circular import, but it is fine as of now
 
 @dataclass
 class Entity:
@@ -10,7 +10,7 @@ class Entity:
     mainly used for the purpose of upcalling the Mininet Adapter from the sub-classes
     """
     isRegister: bool = False
-    mininet_adpator: MininetAdapter = None
+    mininet_adpator = None
     
     def register(self, mininet_adpator):
         self.mininet_adpator = mininet_adpator # For Upper level access, upcalling purpose
