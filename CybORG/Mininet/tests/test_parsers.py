b2cef5ea5e2568a437c36d4c55b412617ef57e28
justinyeh1995 <justinyeh1995@gmail.com>
2024-07-30 20:40:59 -0500

    Update tests/test_parsers.py : the fixture get the real ouput text

--- CybORG/Mininet/tests/test_parsers.py
+++ CybORG/Mininet/tests/test_parsers.py
@@ -10,61 +10,62 @@ from CybORG.Mininet.mininet_adapter.utils.parse_red_results_util import parse_nm
 
 @pytest.fixture
 def discover_network_scan_output():
-    return '''<?xml version="1.0" encoding="UTF-8"?>
+    return ''' lan3h1 timeout 60 nmap -oX - -sn 10.0.37.224/28
+<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE nmaprun>
 <?xml-stylesheet href="file:///usr/bin/../share/nmap/nmap.xsl" type="text/xsl"?>
-<!-- Nmap 7.80 scan initiated Tue Jul 30 19:05:19 2024 as: nmap -oX - -sn 10.0.37.224/28 -->
-<nmaprun scanner="nmap" args="nmap -oX - -sn 10.0.37.224/28" start="1722384319" startstr="Tue Jul 30 19:05:19 2024" version="7.80" xmloutputversion="1.04">
+<!-- Nmap 7.80 scan initiated Tue Jul 30 20:33:36 2024 as: nmap -oX - -sn 10.0.37.224/28 -->
+<nmaprun scanner="nmap" args="nmap -oX - -sn 10.0.37.224/28" start="1722389616" startstr="Tue Jul 30 20:33:36 2024" version="7.80" xmloutputversion="1.04">
 <verbose level="0"/>
 <debugging level="0"/>
 <host><status state="up" reason="arp-response" reason_ttl="0"/>
 <address addr="10.0.37.225" addrtype="ipv4"/>
-<address addr="C6:75:87:2E:AB:FD" addrtype="mac"/>
+<address addr="F6:ED:4C:E4:19:D6" addrtype="mac"/>
 <hostnames>
 </hostnames>
-<times srtt="4890" rttvar="5000" to="100000"/>
+<times srtt="5501" rttvar="5501" to="100000"/>
 </host>
 <host><status state="up" reason="arp-response" reason_ttl="0"/>
 <address addr="10.0.37.226" addrtype="ipv4"/>
-<address addr="E2:4C:B9:D8:F7:B3" addrtype="mac"/>
+<address addr="FE:C6:E0:B2:AB:E1" addrtype="mac"/>
 <hostnames>
 </hostnames>
-<times srtt="4112" rttvar="5000" to="100000"/>
+<times srtt="4128" rttvar="5000" to="100000"/>
 </host>
 <host><status state="up" reason="arp-response" reason_ttl="0"/>
 <address addr="10.0.37.227" addrtype="ipv4"/>
-<address addr="BA:18:E0:A2:F4:B7" addrtype="mac"/>
+<address addr="56:AB:71:1F:B7:03" addrtype="mac"/>
 <hostnames>
 </hostnames>
-<times srtt="4059" rttvar="5000" to="100000"/>
+<times srtt="4114" rttvar="5000" to="100000"/>
 </host>
 <host><status state="up" reason="arp-response" reason_ttl="0"/>
 <address addr="10.0.37.228" addrtype="ipv4"/>
-<address addr="4A:E9:16:26:7A:69" addrtype="mac"/>
+<address addr="42:78:7F:F2:0F:64" addrtype="mac"/>
 <hostnames>
 </hostnames>
-<times srtt="4043" rttvar="5000" to="100000"/>
+<times srtt="4191" rttvar="5000" to="100000"/>
 </host>
 <host><status state="up" reason="arp-response" reason_ttl="0"/>
 <address addr="10.0.37.229" addrtype="ipv4"/>
-<address addr="C2:4F:C8:F2:37:42" addrtype="mac"/>
+<address addr="4E:2B:9F:0D:FD:F9" addrtype="mac"/>
 <hostnames>
 </hostnames>
-<times srtt="4018" rttvar="5000" to="100000"/>
+<times srtt="5236" rttvar="5236" to="100000"/>
 </host>
 <host><status state="up" reason="arp-response" reason_ttl="0"/>
 <address addr="10.0.37.238" addrtype="ipv4"/>
-<address addr="02:48:1E:7A:9C:71" addrtype="mac"/>
+<address addr="72:19:F6:95:25:3D" addrtype="mac"/>
 <hostnames>
 </hostnames>
-<times srtt="2045" rttvar="5000" to="100000"/>
+<times srtt="9319" rttvar="9319" to="100000"/>
 </host>
 <host><status state="up" reason="localhost-response" reason_ttl="0"/>
 <address addr="10.0.37.234" addrtype="ipv4"/>
 <hostnames>
 </hostnames>
 </host>
-<runstats><finished time="1722384345" timestr="Tue Jul 30 19:05:45 2024" elapsed="26.38" summary="Nmap done at Tue Jul 30 19:05:45 2024; 16 IP addresses (7 hosts up) scanned in 26.38 seconds" exit="success"/><hosts up="7" down="9" total="16"/>
+<runstats><finished time="1722389642" timestr="Tue Jul 30 20:34:02 2024" elapsed="26.31" summary="Nmap done at Tue Jul 30 20:34:02 2024; 16 IP addresses (7 hosts up) scanned in 26.31 seconds" exit="success"/><hosts up="7" down="9" total="16"/>
 </runstats>
 </nmaprun>
 '''
