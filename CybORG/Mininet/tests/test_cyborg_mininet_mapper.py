6b536e857c79a3b8ac7ddc9e5b86bbdcadbed25d
justinyeh1995 <justinyeh1995@gmail.com>
2024-08-02 17:09:07 -0500

    Add tests/test_cyborg_mininet_mapper.py : start writing tests for main component in MininetAdapter

---
+++ CybORG/Mininet/tests/test_cyborg_mininet_mapper.py
@@ -0,0 +1,5 @@
+from CybORG import CybORG
+from CybORG.Mininet.CustomFactory import CybORGFactory, AgentFactory
+from CybORG.Mininet.mininet_adapter import CybORGMininetMapper
+
+# Create a CybORG instance
