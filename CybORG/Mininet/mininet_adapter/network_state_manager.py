c2e5ce345db201f115bda6ee606eb23cb30f8fcc
justinyeh1995 <justinyeh1995@gmail.com>
2024-08-06 11:14:23 -0500

    Add mininet_adapter/network_state_manager.py : manage network state for reward calculations

---
+++ CybORG/Mininet/mininet_adapter/network_state_manager.py
@@ -0,0 +1,41 @@
+import logging
+from typing import Dict
+from pydantic import BaseModel, Field
+# from CybORG.Mininet.mininet_adapter.entity import Entity
+
+class NetworkStateManager(BaseModel):
+    
+    info_dict: Dict[str, dict] = Field(default_factory=lambda: dict())
+
+    def update_network_state(self, action_type, server_name, obs, username='root'):
+        if action_type == "PrivilegeEscalate" and obs.success.name == "TRUE":
+            self.update_reward_information_dict(server_name, username)
+        
+        elif action_type == "Remove" and obs.success.name == "TRUE":
+            self.delete_reward_information_dict(server_name, username)
+        
+        elif action_type == "Restore" and obs.success.name == "TRUE":
+            self.delete_reward_information_dict(server_name, username)
+        
+        
+    def update_reward_information_dict(self, server_name, username='root'):
+        if server_name not in self.info_dict:
+            self.info_dict[server_name] = {'Sessions': []}
+        self.info_dict[server_name]['Sessions'].append({'Username': username})
+        
+        return self.info_dict
+
+    def delete_reward_information_dict(self, server_name, username='root'):
+        if server_name in self.info_dict:
+            sessions = self.info_dict[server_name]['Sessions']
+            self.info_dict[server_name]['Sessions'] = [session for session in sessions if session['Username'] != username]
+            
+            # If there are no more sessions, remove the server entry
+            if not self.info_dict[server_name]['Sessions']:
+                del self.info_dict[server_name]
+        
+        return self.info_dict
+    
+    def get_network_state(self):
+        return self.info_dict
+    
