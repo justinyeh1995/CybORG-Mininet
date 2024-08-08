866fdcd1c6a58e01ff183a8107429ce231fd88c5
justinyeh1995 <justinyeh1995@gmail.com>
2024-08-08 18:42:02 -0500

    Update mininet_adapter/network_state_manager.py : add more logging

--- CybORG/Mininet/mininet_adapter/network_state_manager.py
+++ CybORG/Mininet/mininet_adapter/network_state_manager.py
@@ -10,12 +10,15 @@ class NetworkStateManager(BaseModel):
     def update_network_state(self, action_type, server_name, obs, username='root'):
         if action_type == "PrivilegeEscalate" and obs.success.name == "TRUE":
             self.update_reward_information_dict(server_name, username)
+            logging.debug (f"Privilege Escalated on {server_name}, the info dict is now: {self.info_dict}")
         
         elif action_type == "Remove" and obs.success.name == "TRUE":
             self.delete_reward_information_dict(server_name, username)
-        
+            logging.debug (f"Removed {server_name} from the network, the info dict is now: {self.info_dict}")
+
         elif action_type == "Restore" and obs.success.name == "TRUE":
             self.delete_reward_information_dict(server_name, username)
+            logging.debug (f"Restored {server_name} to its original state, the info dict is now: {self.info_dict}")
         
         
     def update_reward_information_dict(self, server_name, username='root'):
