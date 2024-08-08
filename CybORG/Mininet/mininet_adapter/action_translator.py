ad3832ed3546a71b5407a035f8537bbe991cf65b
justinyeh1995 <justinyeh1995@gmail.com>
2024-08-08 18:41:36 -0500

    Updating mininet_adapter/action_translator.py  : add --python-path \'{self.python_exe_filepath}\' --action-folder \'{self.action_folder_path} to ensure flexible config in shell script

--- CybORG/Mininet/mininet_adapter/action_translator.py
+++ CybORG/Mininet/mininet_adapter/action_translator.py
@@ -7,13 +7,21 @@ from CybORG.Mininet.mininet_adapter.entity import Entity
 
 
 class ActionTranslator(Entity):
+    """_summary_
+    Bascially this class is used to translate the actions from CybORG to Mininet, this is the parent class for the Red and Blue Team's action translator
+    It defines the common methods and attributes that the Red and Blue Team's action translator will have
+    It is a child class of the Entity class
+    It has a reset action string method that is used to reset the environment 
+    Args:
+        Entity (_type_): _description_
+    """
     def __init__(self, path: str, config, logger):
         self.logger = logger
         self.path = path
         self.last_action: str = None
         self.last_target: str = None
         self.hostname = socket.gethostname()
-        self.python_exe = config["PYTHON"]["FILE_PATH"]
+        self.python_exe_filepath = config["PYTHON"]["FILE_PATH"]
         self.action_folder_path = self.path + config["ACTION"]["FOLDER_PATH"]
         self.velociraptor_server_mininet_hostname = None
 
@@ -21,12 +29,18 @@ class ActionTranslator(Entity):
         raise NotImplementedError
     
     def get_reset_action_string(self, target_host, cyborg_to_mininet_host_map, mininet_host_to_ip_map):
-        # action = f"{self.python_exe} {self.action_folder_path}/Velociraptor/reset.py"
+        # action = f"{self.python_exe_filepath} {self.action_folder_path}/Velociraptor/reset.py"
         action = f"bash {self.action_folder_path}/ResetAction/reset.sh"
         return f'{cyborg_to_mininet_host_map["Defender"]} {action} --mininet_hostname "{target_host}" ' 
 
 
 class RedActionTranslator(ActionTranslator):
+    """_summary_
+    Bascially this class is used to translate the actions from CybORG to Mininet, this is the Red Team's action translator
+    You can think of this as a string builder for the actions that the Red Team will take in the Mininet environment
+    Args:
+        ActionTranslator (_type_): _description_
+    """
     def __init__(self, path: str, config, logger):
         super().__init__(path, config, logger)  # Correctly calls the __init__ method of ActionTranslator
         self.action_map = {
@@ -74,17 +88,17 @@ class RedActionTranslator(ActionTranslator):
         json_str = json.dumps(additional_data)
         base64_data = base64.b64encode(json_str.encode('utf-8')).decode('utf-8')
         
-        # action = f"{self.python_exe} {self.action_folder_path}/Velociraptor/exploit_action.py"
+        # action = f"{self.python_exe_filepath} {self.action_folder_path}/Velociraptor/exploit_action.py"
         action = f"{self.action_folder_path}/ExploitAction/exploit.sh"
 
         target = mininet_host_to_ip_map.get(target_host, cyborg_to_mininet_host_map['User0'])
         
-        return f'{host} timeout 120 bash {action} -m {target} -data {base64_data}'
+        return f'{host} bash {action} -m {target} -data {base64_data}'
 
     def privilege_escalate(self, target_host, cyborg_to_mininet_host_map, mininet_host_to_ip_map):
         print("Red Privilege Escalate")
         host = cyborg_to_mininet_host_map['User0']
-        # action = f"{self.python_exe} {self.action_folder_path}/Velociraptor/privilege_action.py"
+        # action = f"{self.python_exe_filepath} {self.action_folder_path}/Velociraptor/privilege_action.py"
         action = f"bash {self.action_folder_path}/PrivilegeEscalateAction/privilege.sh"
         target = mininet_host_to_ip_map.get(target_host, cyborg_to_mininet_host_map['User0'])
         conn_key = self.mininet_adpator.connection_key[target]
@@ -92,10 +106,18 @@ class RedActionTranslator(ActionTranslator):
         return f'{host} {action} -m {target} --conn_key {conn_key}' # This might take a while to run so we take out the timeout as a temporary solution
     
     def impact(self, target_host):
-        pass
+        print("Red Impact")
+        # @To-Do Not Implemented as of now
+        return "bash sleep 1"
 
 
 class BlueActionTranslator(ActionTranslator):
+    """_summary_
+    Bascially this class is used to translate the actions from CybORG to Mininet, this is the Blue Team's action translator
+    You can think of this as a string builder for the actions that the Blue Team will take in the Mininet environment
+    Args:
+        ActionTranslator (_type_): _description_
+    """
     def __init__(self, path: str, config, logger):
         super().__init__(path, config, logger)  # Correctly calls the __init__ method of ActionTranslator
         self.decoy_service_name_to_port = {
@@ -133,7 +155,7 @@ class BlueActionTranslator(ActionTranslator):
         print("Blue Remove")
         # @To-Do Not Implemented as of now
         host = cyborg_to_mininet_host_map['Defender']
-        # action = f"{self.python_exe} {self.action_folder_path}/Velociraptor/remove.py"
+        # action = f"{self.python_exe_filepath} {self.action_folder_path}/Velociraptor/remove.py"
         action = f"bash {self.action_folder_path}/RemoveAction/remove.sh"        
         
         target = mininet_host_to_ip_map.get(target_host, cyborg_to_mininet_host_map['User0'])
@@ -142,16 +164,15 @@ class BlueActionTranslator(ActionTranslator):
         # return f"{host} {action} --hostname {self.hostname} --conn_key {conn_key}"
         return f"{host} {action} -n {conn_key}"
 
-
     def restore(self, action_type, target_host, cyborg_to_mininet_host_map, mininet_host_to_ip_map):
         print("Blue Restore")
         # @To-Do Not Implemented as of now
-        return "sleep 1"
+        return "bash sleep 1"
 
     def monitor(self, action_type, target_host, cyborg_to_mininet_host_map, mininet_host_to_ip_map):
         print("Blue Monitor")
         # @To-Do Not Implemented as of now
-        return "sleep 1"
+        return "bash sleep 1"
     
     def sleep(self, action_type, target_host, cyborg_to_mininet_host_map, mininet_host_to_ip_map):
         host = cyborg_to_mininet_host_map['Defender']
@@ -160,7 +181,7 @@ class BlueActionTranslator(ActionTranslator):
     def deploy_decoy(self, action_type, target_host, cyborg_to_mininet_host_map, mininet_host_to_ip_map):
         print("Blue Decoy")
         host = cyborg_to_mininet_host_map['Defender']
-        action = f"{self.python_exe} {self.action_folder_path}/DecoyAction/deploy_decoy_action.py"
+        action = f"{self.python_exe_filepath} {self.action_folder_path}/DecoyAction/deploy_decoy_action.py"
         target = mininet_host_to_ip_map.get(target_host, cyborg_to_mininet_host_map['User0'])
         port = self.decoy_service_name_to_port.get(action_type, 80)
         cyborg_hostname = self.mininet_adpator.mapper.cyborg_ip_to_host_map.get(target)
@@ -169,7 +190,8 @@ class BlueActionTranslator(ActionTranslator):
     
     def analyse(self, action_type, target_host, cyborg_to_mininet_host_map, mininet_host_to_ip_map):
         host = cyborg_to_mininet_host_map['Defender']
-        action  = f"{self.python_exe} {self.action_folder_path}/Velociraptor/analyse.py"
+        # action  = f"{self.python_exe_filepath} {self.action_folder_path}/Velociraptor/analyse.py"
+        action = f"bash {self.action_folder_path}/AnalyseAction/analyse.sh"
         
         hostname = socket.gethostname()
         # Serialize additional data
@@ -179,4 +201,4 @@ class BlueActionTranslator(ActionTranslator):
         json_str = json.dumps(additional_data)
         base64_data = base64.b64encode(json_str.encode('utf-8')).decode('utf-8')
         # @To-Do lan1h4 velociraptor server is not correctly setup and is expected cause the config file specify the ip already
-        return f'{host} {action} --mininet_hostname "{target_host}" --hostname {hostname} --additional_data {base64_data}' 
+        return f'{host} {action} --mininet_hostname \'{target_host}\' --additional_data {base64_data} --python-path \'{self.python_exe_filepath}\' --action-folder \'{self.action_folder_path}\'' 
