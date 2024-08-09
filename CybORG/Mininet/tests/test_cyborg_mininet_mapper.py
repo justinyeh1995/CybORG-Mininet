1b2e7655f3b744f2a4d21baae832031bb0f1006b
justinyeh1995 <justinyeh1995@gmail.com>
2024-08-09 17:03:11 -0500

    Rename tests -> Tests

--- CybORG/Mininet/tests/test_cyborg_mininet_mapper.py
+++
@@ -1,5 +0,0 @@
-from CybORG import CybORG
-from CybORG.Mininet.CustomFactory import CybORGFactory, AgentFactory
-from CybORG.Mininet.mininet_adapter import CybORGMininetMapper
-
-# Create a CybORG instance
--- CybORG/Mininet/tests/test_yaml_topo_generator.py
+++
@@ -1,8 +0,0 @@
-import pytest
-import inspect
-
-from CybORG.Mininet.mininet_adapter import YamlTopologyManager
-
-class TestMininetYamlTopologyManager:
-    def __init__(self):
-        self.yaml_topo_generator = YamlTopologyManager()
