34884c85f5c36d65832b3db5d0596f4834ac5fe5
justinyeh1995 <justinyeh1995@gmail.com>
2024-08-29 11:26:17 -0500

    Remove test_cyborg_mininet_mapper.py : cause there's nothing inside

--- CybORG/Mininet/Tests/test_cyborg_mininet_mapper.py
+++
@@ -1,5 +0,0 @@
-from CybORG import CybORG
-from CybORG.Mininet.CustomFactory import CybORGFactory, AgentFactory
-from CybORG.Mininet.AdapterComponents import CybORGMininetMapper
-
-# Create a CybORG instance
